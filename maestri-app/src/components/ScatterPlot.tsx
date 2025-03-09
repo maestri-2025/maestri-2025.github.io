efb17ca98b575e487c5f25513d713c88a28e3b07
Andrei Daniel Balanica <danielbalanica.dev@gmail.com>
2025-03-09 01:32:25 +0100

    cleanup + use new songs lists in artist view

--- maestri-app/src/components/ScatterPlot.tsx
+++ maestri-app/src/components/ScatterPlot.tsx
@@ -2,73 +2,51 @@
 // yarn add @nivo/scatterplot
 import {ResponsiveScatterPlot} from '@nivo/scatterplot'
 import {getColorPalette, getTheme} from '../utils/colorUtilities';
-import {Artist, Track} from '../utils/interfaces';
+import {Track} from '../utils/interfaces';
 import {Dropdown} from 'primereact/dropdown';
 import {useEffect, useState} from 'react';
-import {CountryDetails} from "../utils/mapUtilities.ts";
 import {Button} from "primereact/button";
-import {DataModel} from "../DataModel.ts";
+import NoDataFoundMessage from "./NoDataFoundMessage.tsx";
 
-function ScatterPlot(props: {model: DataModel, artist: Artist, currentTracks: Array<Track>, currentWeek: string, country: CountryDetails}) {
-
-    if (props.currentTracks.length === 0) {
-        return null;
-    }
-
+function ScatterPlot(props: { currentTracks: Array<Track> }) {
     const axisOptions = [
-        {
-            label: "Team Size",
-            computation: (track: Track) => track.credits.length
-        },
-        {
-            label: "Peak Charting Countries",
-            computation: (track: Track) => (new Set(track.chartings.map((chart) => chart.country))).size
-        },
-        {
-            label: "Peak Rank",
-            computation: (track: Track, country: CountryDetails) => {
-              return Math.min(...track.chartings
-                .filter(chart => country.spotifyCode !== null ? chart.country === country.spotifyCode : true)
-                .map(chart => chart.rank))
-            }
-        },
-        {
-            label: "Weeks on Chart",
-            computation: (track: Track, country: CountryDetails) => {
-              return Math.max(...track.chartings
-                .filter(chart => country.spotifyCode !== null ? chart.country === country.spotifyCode : true)
-                .map(chart => chart.weeks_on_chart))
-            }
-        },
-        {
-            label: "Peak Weekly Streams",
-            computation: (track: Track, country: CountryDetails) => {
-              if (country.spotifyCode !== null) {
-                return Math.max(...track.chartings
-                  .filter(chart => chart.country === country.spotifyCode)
-                  .map(chart => chart.num_streams))
-              }
+    {
+      label: "Team Size",
+      computation: (track: Track) => track.credits.length
+    },
+    {
+      label: "Peak Charting Countries",
+      computation: (track: Track) => (new Set(track.chartings.map((chart) => chart.country))).size
+    },
+    {
+      label: "Peak Rank",
+      computation: (track: Track) => Math.min(...track.chartings.map(chart => chart.rank))
+    },
+    {
+      label: "Weeks on Chart",
+      computation: (track: Track) => Math.max(...track.chartings.map(chart => chart.weeks_on_chart))
+    },
+    {
+      label: "Peak Weekly Streams",
+      computation: (track: Track) => {
+        const streamsPerWeek = new Map<string, number>();
+        track.chartings
+          .forEach(chart => streamsPerWeek.set(chart.week, (streamsPerWeek.get(chart.week) ?? 0) + chart.num_streams))
 
-              const streamsPerWeek = new Map<string, number>();
-              track.chartings
-                .filter(chart => chart.country !== "GLOBAL")
-                .forEach(chart => streamsPerWeek.set(chart.week, (streamsPerWeek.get(chart.week) ?? 0) + chart.num_streams))
+        return Math.max(...Array.from(streamsPerWeek.values()))
+      }
+    },
+    {
+      label: "Samples/Interpolations",
+      computation: (track: Track) => track.stats.samples + track.stats.interpolations
+    }
+  ]
 
-              return Math.max(...Array.from(streamsPerWeek.values()))
-            }
-        },
-        {
-          label: "Samples/Interpolations",
-          computation: (track: Track) => {
-            console.log(track)
-            return track?.stats?.samples + track?.stats?.interpolations
-          }
-        }
-    ]
     const [xAxis, setXAxis] = useState(axisOptions[0]);
     const [yAxis, setYAxis] = useState(axisOptions[1]);
-
     const [data, setData] = useState<{ id: string; data: { x: number; y: number; }[]; }[]>([]);
+
+
 
     function buildData() {
       return props.currentTracks.map(track => (
@@ -76,8 +54,8 @@ function ScatterPlot(props: {model: DataModel, artist: Artist, currentTracks: Ar
           id: track.name,
           data: [
             {
-              "x": xAxis.computation(track, props.country),
-              "y": yAxis.computation(track, props.country),
+              "x": xAxis.computation(track),
+              "y": yAxis.computation(track),
             }
           ]
         }
@@ -85,14 +63,13 @@ function ScatterPlot(props: {model: DataModel, artist: Artist, currentTracks: Ar
     }
 
     useEffect(() => {
-        const data = buildData()
-        setData(data)
-    }, [xAxis, yAxis, props.country]);
-
+      setData(buildData())
+    }, [xAxis, yAxis, props.currentTracks]);
 
     return (
         <div style={{height: '100%'}}>
-          <div style={{height: "90%"}}>
+          <div style={{height: "90%", position: "relative"}}>
+            { data.length === 0 && <NoDataFoundMessage message="Try changing data selection"></NoDataFoundMessage> }
             <ResponsiveScatterPlot
               data={data}
               margin={{ top: 25, right: 25, bottom: 70, left: 70 }}
